8d4e548fe897a01c8341c1b4f1fa1d2bf40e02b6
Henrylette <47320247+Henrylette@users.noreply.github.com>
2021-11-08 13:12:15 +0100

    Upload
    Filer

---
+++ AAO-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AAO_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
