ad5ecacef0cc6821738053e2d79a6a77c7af6989
nikolairaahauge <nira36696@edu.ucl.dk>
2021-11-12 11:50:11 +0100

    Removed bootstrap Removed jQuery Set up files Set up directories

--- AAO-App/Startup.cs
+++ AAO-App/Startup.cs
@@ -50,7 +50,7 @@ namespace AAO_App
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
