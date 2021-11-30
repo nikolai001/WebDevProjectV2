e24bb0583bbd9836a034634331dc12135f4f5a5a
Mikkel Jørgen Bang <mjba36697@edu.ucl.dk>
2021-11-30 19:42:53 +0100

    Har prøvet at få data fra databasen men min måde at gøre det på virker ikk...

--- AAO-App/Controllers/ProfileController.cs
+++ AAO-App/Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AAO_App.Data;
+using AAO_App.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -20,9 +21,18 @@ namespace AAO_App.Controllers
             _db = db;
         }
 
+        
         // GET: /<controller>/
         public IActionResult Index()
         {
+            /*var DriverID = Driver.Identity.GetDriverId();
+
+            Profile.Firstname = Driver.FirstName;
+            Profile.Address = Driver.Address;
+            Profile.Location = Driver.Location;
+            Profile.DriverLicensType = Driver.DriverLicensType;
+            */
+
             return View();
         }
 
