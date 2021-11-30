05273cb2c78fd22cf64cb4eaea6fc31783a87245
henrietteabbednaes <henr8588@edu.ucl.dk>
2021-11-30 23:31:46 +0100

    Tilføjet TripController med indhold

--- AAO-App/Controllers/TripController.cs
+++ AAO-App/Controllers/TripController.cs
@@ -3,17 +3,165 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AAO_App.Data;
+using AAO_App.Models;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace AAO_App.Controllers
+namespace AAO_App
 {
     public class TripController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public TripController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        // GET: Trip
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Trips.Include(t => t.Cities).Include(t => t.Drivers);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Trip/Details/5
+        [Route("3")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trips
+                .Include(t => t.Cities)
+                .Include(t => t.Drivers)
+                .FirstOrDefaultAsync(m => m.TripId == id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return View(trip);
+        }
+
+        // GET: Trip/Create
+        public IActionResult Create()
+        {
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityId");
+            ViewData["DriverId"] = new SelectList(_context.Drivers, "DriverId", "DriverId");
             return View();
+        }
+
+        // POST: Trip/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TripId,DriverId,CityId,EmpId,DateStart,DateEnd,MessageContents,TripInfo,DriverBuddy")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(trip);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityId", trip.CityId);
+            ViewData["DriverId"] = new SelectList(_context.Drivers, "DriverId", "DriverId", trip.DriverId);
+            return View(trip);
+        }
+
+        // GET: Trip/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trips.FindAsync(id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityId", trip.CityId);
+            ViewData["DriverId"] = new SelectList(_context.Drivers, "DriverId", "DriverId", trip.DriverId);
+            return View(trip);
+        }
+
+        // POST: Trip/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TripId,DriverId,CityId,EmpId,DateStart,DateEnd,MessageContents,TripInfo,DriverBuddy")] Trip trip)
+        {
+            if (id != trip.TripId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(trip);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TripExists(trip.TripId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityId", trip.CityId);
+            ViewData["DriverId"] = new SelectList(_context.Drivers, "DriverId", "DriverId", trip.DriverId);
+            return View(trip);
+        }
+
+        // GET: Trip/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trips
+                .Include(t => t.Cities)
+                .Include(t => t.Drivers)
+                .FirstOrDefaultAsync(m => m.TripId == id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return View(trip);
+        }
+
+        // POST: Trip/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var trip = await _context.Trips.FindAsync(id);
+            _context.Trips.Remove(trip);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TripExists(int id)
+        {
+            return _context.Trips.Any(e => e.TripId == id);
         }
     }
 }
