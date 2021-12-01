2505c4b0db1137891f85129f8a6ffb96aaa0e31c
edna <edna@LAPTOP-9PK5LSTL>
2021-12-01 20:28:46 +0100

    Correcting shit

---
+++ AAO-App/wwwroot/js/DriverHistory.js
@@ -0,0 +1,31 @@
+﻿function ShowAlert() {
+    Swal.fire({
+        title: '<h2 style="font-size: 16px;line-height: 1.6rem;font-weight: bolder;">Er du sikker på at du vil slette din profil?</h2>',
+        //icon: 'info',
+        html:
+            '<div class="radio-container">' +
+            '<label for="check">' +
+            '<input id="check" type="checkbox" class= "checkbox-round" checked="checked" >' +
+            'Tryk for at slette profil' +
+            '</label>' +
+            '</div>',
+        showDenyButton: true,
+        showCancelButton: false,
+        confirmButtonText: 'Slet',
+        confirmButtonColor: '#00904a',
+        denyButtonText: `Annuler`,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: '<p style="font-size: 16px;">Din profil er nu slettet </p>',
+                showDenyButton: true,
+                showCancelButton: false,
+                showConfirmButton: false,
+                //confirmButtonText: 'Annuler',
+                denyButtonText: `Slet`,
+            })
+        }
+    })
+
+}
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
