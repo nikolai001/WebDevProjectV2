a174ba94a4c1394150c138307ba4e2e97b45cee5
henrietteabbednaes <henr8588@edu.ucl.dk>
2021-12-03 00:51:08 +0100

    Prøvet på at rette js

--- AAO-App/wwwroot/js/TripJs.js
+++ AAO-App/wwwroot/js/TripJs.js
@@ -30,12 +30,41 @@ function AvaTrip() {
 }
 
 /*VISER INFORMATIONER OM TUREN*/
-document.getElementById("TripHolder").addEventListener("click", ShowInfo); 
 
+/*document.getElementById("TripContainer").addEventListener('click', (e) => {
+    console.log(e.target.id);
+});
+*/
+
+
+var Info = document.getElementsByClassName("MoreInfo")[0];
+for (var i = 0; i < Info.length; i++) {
+    Info[i].id = "Info" + (i + 1);
+}
+
+var Date = document.getElementsByClassName("Date")[0];
+for (var i = 0; i < Date.length; i++) {
+    Date[i].id = "Date" + (i + 1);
+}
+
+var Country = document.getElementsByClassName("Country")[0];
+for (var i = 0; i < Country.length; i++) {
+    Country[i].id = "Country" + (i + 1);
+}
+
+var Holder = document.getElementsByClassName("TripsHolder")[0];
+for (var i = 0; i < Holder.length; i++) {
+    Holder[i].id = "TripHolder" + (i + 1);
+}
+
+
+Holder.addEventListener('click', ShowInfo);
 function ShowInfo() {
-    var Info = document.getElementById("Info");
-    var Date = document.getElementById("Date");
-    var Country = document.getElementById("Country");
+    /*var Info = document.getElementById("Info")[1];
+    var Date = document.getElementById("Date")[1];
+    var Country = document.getElementById("Country")[1];*/
+    //for (var i = 0; i < Info.length; i++) {
+
 
     if (Info.style.display == "none" || Date.style.borderRadius == "0.25em 0 0 0.25em" || Country.style.borderRadius == "0 0 0.25em 0") {
         Info.style.display = "grid";
