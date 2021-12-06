0de1961ed35e57a09f32e7371fa1a7a704ff914b
nikolairaahauge <nira36696@edu.ucl.dk>
2021-12-06 12:26:47 +0100

    Swallah

---
+++ AAO-App/wwwroot/js/Modal.js
@@ -0,0 +1,15 @@
+﻿const Scenarios = {
+
+}
+
+let Button = document.querySelector(".ModalTrigger");
+Button.addEventListener('click', function (e) {
+    var targetId = e.target.getAttribute('id');
+    if (targetId == "Help") {
+        var Modal = document.querySelector("#Modal");
+        Modal.querySelector("#ModalTitle").innerHTML = Scenarios[0]
+        Modal.querySelector("#ModalContent").innerHTML = Scenarios[0]
+        Modal.style.Display = "grid"
+        //Modal.classList.add('Display');
+    }
+});
