b694064f2523ca66e35bef02c808c1a6f13b3e40
nikolairaahauge <nira36696@edu.ucl.dk>
2021-12-06 13:23:55 +0100

    Flaf

--- AAO-App/wwwroot/js/Modal.js
+++ AAO-App/wwwroot/js/Modal.js
@@ -1,15 +1,15 @@
-﻿const Scenarios = {
-
-}
+﻿const Scenarios = [
+    { Name: "Help", Title: "Få hjælp til login", Content: "Ring til telefon nr. <br> +45 12 13 14 15" },
+];
 
 let Button = document.querySelector(".ModalTrigger");
 Button.addEventListener('click', function (e) {
     var targetId = e.target.getAttribute('id');
     if (targetId == "Help") {
         var Modal = document.querySelector("#Modal");
-        Modal.querySelector("#ModalTitle").innerHTML = Scenarios[0]
-        Modal.querySelector("#ModalContent").innerHTML = Scenarios[0]
-        Modal.style.Display = "grid"
+        Modal.querySelector("#ModalTitle").innerHTML = Scenarios[0].Title
+        Modal.querySelector("#ModalContent").innerHTML = Scenarios[0].Content
+        Modal.classList.add('Display');
         //Modal.classList.add('Display');
     }
 });
