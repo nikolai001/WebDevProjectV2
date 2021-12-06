24c64bc9d1884640253b5232a13115697d6c9ee3
henrietteabbednaes <henr8588@edu.ucl.dk>
2021-12-06 11:35:38 +0100

    Rettet model DepartmentHasEmployee

---
+++ AAO-App/Models/DepartmentHasEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AAO_App.Models
+{
+    public class DepartmentHasEmployee
+    {
+        [Key]
+        public int DepartmentHasEmployeesId { get; set; }
+        public int DepId { get; set; }
+        public Department Departments { get; set; }
+        public int EmppId { get; set; }
+        public Employee Employees { get; set; }
+    }
+}
