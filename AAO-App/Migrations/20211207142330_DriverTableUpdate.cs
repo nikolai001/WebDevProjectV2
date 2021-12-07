aa17da423e8f6bf03fe7497e106799a7b85cee37
henrietteabbednaes <henr8588@edu.ucl.dk>
2021-12-07 15:32:29 +0100

    Rette Driver table Har rette driver table og tilføjet ekstra cloumns - kan ikke tilføje foreign keys

---
+++ AAO-App/Migrations/20211207142330_DriverTableUpdate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AAO_App.Migrations
+{
+    public partial class DriverTableUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IsValidated",
+                table: "Drivers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "LicensImage",
+                table: "Drivers",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Drivers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfileImage",
+                table: "Drivers",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drivers_CityId",
+                table: "Drivers",
+                column: "CityId");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Drivers_Cities_CityId",
+                table: "Drivers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Drivers_CityId",
+                table: "Drivers");
+
+            migrationBuilder.DropColumn(
+                name: "IsValidated",
+                table: "Drivers");
+
+            migrationBuilder.DropColumn(
+                name: "LicensImage",
+                table: "Drivers");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Drivers");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImage",
+                table: "Drivers");
+        }
+    }
+}
--- AAO-App/Models/Driver.cs
+++ AAO-App/Models/Driver.cs
@@ -13,17 +13,18 @@ namespace AAO_App.Models
         public int DriverId { get; set; }
         public int LoginId { get; set; }
         public Login UserLogin { get; set; }
-        public int DriverLicensType { get; set; }
-        public DriverLicensType DriverLicensTypes { get; set; }
         public int CityId { get; set; }
-
+      
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
         public string Location { get; set; }
         public DateTime Birthday { get; set; }
-
+        public string Password { get; set; }
+        public int IsValidated { get; set; }
+        public Byte[] ProfileImage { get; set; }
+        public Byte[] LicensImage { get; set; }
 
     }
 }
