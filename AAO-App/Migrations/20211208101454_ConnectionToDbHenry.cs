c0bd7bf52ebf487c0e7a5f6b1dca7c8f91617cc4
henrietteabbednaes <henr8588@edu.ucl.dk>
2021-12-08 11:16:28 +0100

    Try to connec to DbCloud

---
+++ AAO-App/Migrations/20211208101454_ConnectionToDbHenry.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AAO_App.Migrations
+{
+    public partial class ConnectionToDbHenry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
