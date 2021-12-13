98c0ad94cf3f474056e6a07bb44a352acb4671e9
nikolairaahauge <nira36696@edu.ucl.dk>
2021-12-13 10:47:52 +0100

    Så uploader hun sateme data

--- AAO-App/Controllers/NewUserController.cs
+++ AAO-App/Controllers/NewUserController.cs
@@ -33,7 +33,7 @@ namespace AAO_App.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("DriverId,CityId,Firstname,Lastname,Address,Phone,Location,Birthday,Password,IsValidated,ProfileImage")] Driver driver)
+        public async Task<IActionResult> Index([Bind("DriverId,CityId,Firstname,Lastname,Address,Phone,Location,Birthday,Password,IsValidated,ProfileImage")] Driver driver)
         {
             if (ModelState.IsValid)
             {
@@ -45,72 +45,6 @@ namespace AAO_App.Controllers
             ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityId", driver.CityId);
             ViewData["CityName"] = new SelectList(_context.Cities, nameof(City.CityId), nameof(City.CityName), driver.CityId);
             return View(driver);
-        }
-
-        // POST: DriverTest/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("DriverId,CityId,Firstname,Lastname,Address,Phone,Location,Birthday,Password,IsValidated,ProfileImage")] Driver driver)
-        {
-            if (id != driver.DriverId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(driver);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!DriverExists(driver.DriverId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityId", driver.CityId);
-            return View(driver);
-        }
-
-        // GET: DriverTest/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var driver = await _context.Drivers
-                .Include(d => d.Cities)
-                .FirstOrDefaultAsync(m => m.DriverId == id);
-            if (driver == null)
-            {
-                return NotFound();
-            }
-
-            return View(driver);
-        }
-
-        // POST: DriverTest/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var driver = await _context.Drivers.FindAsync(id);
-            _context.Drivers.Remove(driver);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool DriverExists(int id)
