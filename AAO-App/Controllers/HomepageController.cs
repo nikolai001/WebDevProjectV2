58e73b60be3436b12e6a0929f95fcdb7cf5d0f53
nikolairaahauge <nira36696@edu.ucl.dk>
2021-12-14 18:14:03 +0100

    Working on homepage... it dont work no more

--- AAO-App/Controllers/HomepageController.cs
+++ AAO-App/Controllers/HomepageController.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using AAO_App.Data;
+using AAO_App.Models;
+using Microsoft.EntityFrameworkCore;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace AAO_App.Controllers
 {
     public class HomepageController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HomepageController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        //GET: /<controller>/
+        public IActionResult Index(Trip model)
+        {
+            var TripModelData = (from t in _context.Trips
+                                 orderby t.TripId ascending
+                                 select new Trip
+                                 {
+                                     TripId = t.TripId,
+                                     DateStart = t.DateStart,
+                                     DateEnd = t.DateEnd,
+                                     MessageContents = t.MessageContents,
+                                     TripInfo = t.TripInfo
+                                 }).Take(2).ToList();
+            return View(TripModelData);
         }
     }
 }
