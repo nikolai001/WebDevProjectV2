584055baf8676d47c0d700d0e1080c137302d978
3abooda <abd_s4d@hotmail.com>
2021-12-15 15:46:33 +0100

    * SettingsController.cs:
    * Index.cshtml: slet profile funnktion tilføjet

--- AAO-App/Controllers/SettingsController.cs
+++ AAO-App/Controllers/SettingsController.cs
@@ -1,16 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AAO_App.Data;
+using AAO_App.Models;
+//using BC = BCrypt.Net.BCrypt;
 
 namespace AAO_App.Controllers
 {
     public class SettingsController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public SettingsController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: Settings/Delete/1
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var driver = await _context.Drivers
+                .Include(d => d.Cities)
+                .FirstOrDefaultAsync(m => m.DriverId == id);
+            if (driver == null)
+            {
+                return NotFound();
+            }
+
+            return View(driver);
+        }
+
+        // POST: Settings/Delete/1
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var driver = await _context.Drivers.FindAsync(id);
+            _context.Drivers.Remove(driver);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DriverExists(int id)
+        {
+            return _context.Drivers.Any(e => e.DriverId == id);
         }
     }
 }
